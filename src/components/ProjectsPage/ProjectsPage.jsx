68cf68778eebec420875fbf0591283a4a733ab1c
ESWARARAO-MAKER <balagaeswararao2002@gmail.com>
2024-01-02 15:22:13 +0530

    projects photos

--- src/components/ProjectsPage/ProjectsPage.jsx
+++ src/components/ProjectsPage/ProjectsPage.jsx
@@ -16,14 +16,14 @@ const ProjectsPage = () => {
             <div className="projects-container">
                 <div className="project-card" style={{"--i":1}}>
                     <span>COVID DASHBOARD</span>
-                    <a href="https://eswar-covid-dash-board.netlify.app/" rel="noreferrer" target="_blank"><img src="/image/p2.png" alt= "img" className="covid-img"/></a>
+                    <a href="https://eswar-covid-dash-board.netlify.app/" rel="noreferrer" target="_blank"><img src="/image/Screenshot 2024-01-02 at 3.15.15 PM.png" alt= "img" className="covid-img"/></a>
                     <p>Developed an entire COVID dashboard website, which shows COVID data all over India.</p>
                     <p>Designed front end using REACT which includes fetch data from an internal server using a class component, displaying that data, using component life cycle methods, routing concepts and adding responsiveness to the website.</p>
                     <p>Used third party packages to show charts and loaders and data is fetched from third party api's.</p>
                 </div>
                 <div className="project-card" style={{"--i":2}}>
                     <span>Hotel Booking Application</span>
-                    <a href="https://eswar-hotel-booking.netlify.app/" rel="noreferrer" target="_blank"><img src="/image/img/p3.jpg" alt= "img" className="covid-img"/></a>
+                    <a href="https://eswar-hotel-booking.netlify.app/" rel="noreferrer" target="_blank"><img src="/image/Screenshot 2024-01-02 at 3.16.26 PM.png" alt= "img" className="covid-img"/></a>
                     <p>It is a tool that allows hotel guests to schedule the dates of their stay, choose rooms at the time of booking.</p>
                     <p>Customers can view and book room online for particular place. Admin has the power of either approving or disapproving the customer's booking request.</p>
                     <p>Used third party Packages for authenticationa and for hashing.</p>
